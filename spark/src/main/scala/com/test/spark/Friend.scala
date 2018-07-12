b60b862725a1f81cca7dab403462a8782a571e5c
fenshi <fenshi2009@gmail.com>
2018-07-11 21:52:33 -0400

    fentest1

--- spark/src/main/scala/com/test/spark/Friend.scala
+++ spark/src/main/scala/com/test/spark/Friend.scala
@@ -3,6 +3,6 @@ package com.test.spark
 trait Friend {
   
    val name: String
-   def listen() = println("I " + name + " listening aaaaa")
+   def listen() = println("I " + name + " listening aaaaabbbbb")
    
 }
